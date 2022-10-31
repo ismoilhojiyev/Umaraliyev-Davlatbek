a7218cec724c35dba6050615e56250e366e90d11
ismoilhojiyev <99882047+ismoilhojiyev@users.noreply.github.com>
2022-10-31 16:48:06 +0500

    Initial commit

---
+++ 1Dars.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 16:08:58 2022
+
+@author: User 2
+"""
+# print("Hello World!")
+# print("Assalomu alaykum")
+# print("Xush kelibsiz")
+
+# a=int(input("a sonni kiriting: "))
+# b=int(input("b sonni kiriting: "))
+# g=int(input("g sonni kiriting" ))
+# print(a - b + g)
+
+# ism = input("Assalomu alaykum foydalanuvchi ismingizni kiriting: " )
+# print("Assalomu alaykum ", ism, "dasturimizga xush kelibsiz! ")
+
+# name = "Umaraliyev Davlatbek"
+# age = 14
+# oy = 7
+
+# print(name, "yoshim", age, "da", oy, "- oyda tug'ilganman", )
+ism = input("Assalomu alaykum foydalanuvchi ismingicni kiriting: ")
+yosh = input("Iltimos", ism, "yoshingizni kiriting")
+print
