79f42bac5efe6eceb170c14691f2b3354e2bf922
ismoilhojiyev <99882047+ismoilhojiyev@users.noreply.github.com>
2022-10-31 17:24:28 +0500

    while

---
+++ 13 - dars.py
@@ -0,0 +1,32 @@
+# son = 1
+# while son < 100:
+#     print(son ,end= "\n")
+#     son = son + 1
+# print("salom")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
